6a26b32e861230fffb1869e72829f1b1dc707c08
Adria Mata <amata.dam@institutcampalans.net>
2021-11-05 17:07:04 +0100

    creado .js

---
+++ script.js
@@ -0,0 +1,29 @@
+zonaToDo = document.getElementsByClassName("seccio1");
+zonaDoing = document.getElementsByClassName("seccio2");
+zonaDone = document.getElementsByClassName("seccio3");
+crear = document.getElementsByClassName("afegir");
+nota = document.getElementsById("nota");
+editar = document.getElementsByClassName("editar");
+eliminar = document.getElementsByClassName("eliminar");
+
+
+
+
+
+
+
+
+
+// color = document.getElementById("nota").innerHTML;
+
+// if (color.style.background-color == #592941){
+//     document.getElementById("nota").style.background-color = "#0000FF";
+//     color.getElementById("")
+// }
+// if (color == "rojo"){
+//     document.getElementById("color").style.background-color = "#FF0000";
+// }
+
+// rgb(89,41,65,0.7); => #592941
+// rgb(178, 211, 168,0.7); => #B2D3A8
+// rgb(237, 229, 166,0.7); => #EDE5A6
